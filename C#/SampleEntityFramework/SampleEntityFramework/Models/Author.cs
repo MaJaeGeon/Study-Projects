693000c2b6fb6b8e129bc935a007045d4f070b23
MaJaeGeon <akworjs0517@gmail.com>
2020-12-16 21:25:37 +0900

    EntityFramework Study

---
+++ C#/SampleEntityFramework/SampleEntityFramework/Models/Author.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SampleEntityFramework.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDay { get; set; }
+        public string Gender { get; set; }
+        public virtual ICollection<Book> Books { get; set; }    // Entity 클래스에 다른 Entity 클래스가 프로퍼티로 포함될 경우 해당 프로퍼티는 virtual 로 지정해야한다.
+    }
+}
---
+++ C#/SampleEntityFramework/SampleEntityFramework/Models/BooksDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleEntityFramework.Models
+{
+    class BooksDbContext : DbContext
+    {
+        public BooksDbContext() : base("name=BooksDbContext")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<BooksDbContext, Configuration>());
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+    }
+}
