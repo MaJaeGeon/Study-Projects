bb1e72cc5b9d19cf921f3e48890b6c7dc1397096
MaJaeGeon <akworjs0517@gmail.com>
2021-01-24 23:21:45 +0900

    Binding
    DataContext & Static Resource Binding

---
+++ WPF/Example/Binding/DataContextBinding.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Binding
+{
+    /// <summary>
+    /// DataContextBinding.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class DataContextBinding : Window
+    {
+        public DataContextBinding()
+        {
+            InitializeComponent();
+
+            this.DataContext = new Profile { Name = "MaJaeGeon", Age = 20, Birth = new DateTime(2002, 05, 17) };
+        }
+    }
+}
---
+++ WPF/Example/Binding/Profile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binding
+{
+    public class Profile
+    {
+        public string   Name    { get; set; }
+        public int      Age     { get; set; }
+        public DateTime Birth   { get; set; }
+    }
+}
