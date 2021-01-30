a41bad3611d4fc758ed770ea077fc991691afcdf
MaJaeGeon <akworjs0517@gmail.com>
2021-01-30 22:37:27 +0900

    DataTemplate

---
+++ WPF/Example/DataTemplate/Profile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTemplate
+{
+    public class Profile
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime Birth { get; set; }
+    }
+
+    public class ProfileList : List<Profile> { }
+}
